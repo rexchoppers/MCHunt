821ef69e29ca5e632974964a9acfd231ea6d30b7
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2021-10-31 12:20:41 +0000

    ADDED Repository link

---
+++ src/main/java/codox/uk/mchunt/MCHunt.java
@@ -0,0 +1,16 @@
+package codox.uk.mchunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCHunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
