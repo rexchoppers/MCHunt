cca1437f7ef6c5facf93b1c3fb5c4f65c606e58e
Connor Brady <connorbrady@codox.eu>
2021-11-03 21:44:05 +0000

    WIP Adding main bits of the plugin

--- src/main/java/codox/uk/mchunt/MCHunt.java
+++ src/main/java/codox/uk/mchunt/MCHunt.java
@@ -1,13 +1,29 @@
 package codox.uk.mchunt;
 
 import codox.uk.mchunt.util.GeneralUtility;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCHunt extends JavaPlugin {
 
+    private MCHunt plugin;
+
     @Override
     public void onEnable() {
+        plugin = this;
+
         GeneralUtility.sendConsoleMessage("Starting plugin");
+
+        // Find a decent way to store the data
+        //
+
+        // Init all arenas
+        // Start game loop
+
+        // Run a thread every 1 second
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () ->
+                Bukkit.getConsoleSender().sendMessage("CAlled"), 0, 20L);
     }
 
     @Override
