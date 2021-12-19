264fd6fa49866ed0d98f7c14fcac6508a8648c72
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2021-12-19 12:17:16 +0000

    WIP Adding arena base data

--- src/main/java/codox/uk/mchunt/objects/Arena.java
+++ src/main/java/codox/uk/mchunt/objects/Arena.java
@@ -1,10 +1,15 @@
 package codox.uk.mchunt.objects;
 
+import org.bukkit.Location;
+
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Arena {
 
     private String id;
+    private String name;
+    private ArrayList<Location> signLocations;
 
     public Arena() {
         this.id = UUID.randomUUID().toString();
