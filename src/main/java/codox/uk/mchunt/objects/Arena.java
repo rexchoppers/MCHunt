3374a4674c12390814a5902f361704725849e44f
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2021-12-19 12:37:49 +0000

    ADDED Some basic fields for the arena

--- src/main/java/codox/uk/mchunt/objects/Arena.java
+++ src/main/java/codox/uk/mchunt/objects/Arena.java
@@ -1,17 +1,35 @@
 package codox.uk.mchunt.objects;
 
+import codox.uk.mchunt.enums.ArenaStatusEnum;
+import codox.uk.mchunt.enums.PlayerRoleEnum;
 import org.bukkit.Location;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class Arena {
 
     private String id;
     private String name;
+    private ArenaStatusEnum status;
+
+    // Cannot be less than 2 or more than the max
+    private int minimumPlayers;
+
+    // Cannot be less than min
+    private int maximumPlayers;
+
+    private Location seekerSpawnLocation;
+    private Location hiderSpawnLocation;
     private ArrayList<Location> signLocations;
 
-    public Arena() {
-        this.id = UUID.randomUUID().toString();
-    }
+    private Location boundryPosition1;
+    private Location boundryPosition2;
+
+    private HashMap<UUID, PlayerRoleEnum> players;
+
+    public Arena() {}
+
+    
 }
