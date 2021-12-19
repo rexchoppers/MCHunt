2aaa66671b17d4590e79d652c3ed7fdbc4cbf983
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2021-12-19 13:06:36 +0000

    ADDED Admin menu

--- src/main/java/codox/uk/mchunt/menus/MenuAdmin.java
+++ src/main/java/codox/uk/mchunt/menus/MenuAdmin.java
@@ -2,39 +2,30 @@ package codox.uk.mchunt.menus;
 
 import codox.uk.mchunt.MCHunt;
 import codox.uk.mchunt.MCHuntItems;
-import codox.uk.mchunt.enums.Permissions;
 import fr.minuskube.inv.ClickableItem;
 import fr.minuskube.inv.SmartInventory;
 import fr.minuskube.inv.content.InventoryContents;
 import fr.minuskube.inv.content.InventoryProvider;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-public class MenuMain extends BaseMenu {
-    public MenuMain() {
+public class MenuAdmin extends BaseMenu {
+    public MenuAdmin() {
         this.setInventory(
                 SmartInventory.builder()
-                        .id("mchuntMain")
+                        .id("mchuntMenuAdmin")
                         .manager(MCHunt.getInventoryManagerInstance())
-                        .provider(new MenuMainProvider())
+                        .provider(new MenuAdmin.MenuAdminProvider())
                         .size(3, 9)
-                        .title("MCHunt Menu")
+                        .title("MCHunt - Admin Menu")
                         .closeable(true)
                         .build()
         );
     }
 
-    private class MenuMainProvider implements InventoryProvider {
+    private class MenuAdminProvider implements InventoryProvider {
         @Override
         public void init(Player player, InventoryContents contents) {
-            contents.fillBorders(ClickableItem.empty(new ItemStack(Material.BLACK_STAINED_GLASS_PANE)));
 
-            if (player.hasPermission(MCHuntItems.ITEM_MENU_ADMIN_SECTION.getPermission())) {
-                contents.set(1, 7, ClickableItem.of(MCHuntItems.ITEM_MENU_ADMIN_SECTION.build(), e -> {
-                    (new MenuAdmin()).open(player);
-                }));
-            }
         }
 
         @Override
