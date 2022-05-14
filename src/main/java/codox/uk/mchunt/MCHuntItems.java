6f80e75684ff72fe9f5e0c457481c753fb5d0ac9
Connor Brady <connorbrady@codox.eu>
2022-05-14 16:03:14 +0100

    UPDATED Some of the items to include the string format function

--- src/main/java/codox/uk/mchunt/MCHuntItems.java
+++ src/main/java/codox/uk/mchunt/MCHuntItems.java
@@ -2,6 +2,7 @@ package codox.uk.mchunt;
 
 import codox.uk.mchunt.enums.Permissions;
 import codox.uk.mchunt.items.ItemBuilder;
+import codox.uk.mchunt.util.Format;
 import org.bukkit.Material;
 
 import java.util.ArrayList;
@@ -10,17 +11,17 @@ public class MCHuntItems {
     public static final ItemBuilder ITEM_MENU_ADMIN_SECTION = new ItemBuilder()
             .setMaterial(Material.CRAFTING_TABLE)
             .setAmount(1)
-            .setName("%B&6Enter Admin Menu")
+            .setName(Format.processString("%B&6Enter Admin Menu"))
             .setPermission(Permissions.PERMISSION_ADMIN.toString())
             .setLores(new ArrayList<String>() {{
                 add("");
-                add("%b%tClick to enter the admin menu");
+                add(Format.processString("%b%tClick to enter the admin menu"));
             }});
 
     public static final ItemBuilder ITEM_MENU_BACK = new ItemBuilder()
             .setMaterial(Material.REDSTONE_TORCH)
             .setAmount(1)
-            .setName("%BBack")
+            .setName(Format.processString("%BBack"))
             .setLores(new ArrayList<String>() {{
                 add("");
             }});
