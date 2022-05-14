5b626cf6526c6a5657e1d7cb23108cadcd5dc460
Connor Brady <connorbrady@codox.eu>
2022-05-14 15:40:47 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ src/main/java/codox/uk/mchunt/Permissions.java
@@ -0,0 +1,5 @@
+package codox.uk.mchunt;
+
+public class Permissions {
+    public static String PERMISSION_ADMIN = "mchunt.admin";
+}
---
+++ src/main/java/codox/uk/mchunt/menus/MenuManager.java
@@ -0,0 +1,12 @@
+package codox.uk.mchunt.menus;
+
+import codox.uk.mchunt.MCHunt;
+
+public class MenuManager {
+
+    private MCHunt plugin;
+    
+    public MenuManager() {
+
+    }
+}
