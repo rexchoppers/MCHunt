5f5c2ba0696e0a07d9506e35987380d96ed3cded
Connor Brady <connorbrady@codox.eu>
2022-05-14 16:55:26 +0100

    WIP Adding stash player's inventory

--- src/main/java/codox/uk/mchunt/processor/ArenaSetupProcessor.java
+++ src/main/java/codox/uk/mchunt/processor/ArenaSetupProcessor.java
@@ -1,7 +1,10 @@
 package codox.uk.mchunt.processor;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import com.google.gson.Gson;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.*;
 
 public class ArenaSetupProcessor {
 
@@ -10,8 +13,48 @@ public class ArenaSetupProcessor {
     public ArenaSetupProcessor() {
         this.playersInSetupMode = new ArrayList<>();
     }
-    
-    public void loadFromTemporaryFile() {
+
+    /**
+     */
+    public void loadPlayersFromTemporaryFiles() {
+
+    }
+
+    public ArrayList<UUID> getPlayersInSetupMode() {
+        return this.playersInSetupMode;
+    }
+
+    /**
+     * Dump a player's information into the temporary file
+     */
+    public void stashPlayerInformation(Player player) {
+        HashMap<String, Object> playerInformation = new HashMap<>();
+        List<Map<String, Object>> playerItems = new ArrayList<>();
+        String uuid = player.getUniqueId().toString();
+
+        // Add the player's UUID
+        playerInformation.put("uuid", uuid);
+
+        // Serialize the player's inventory
+        for(ItemStack item: player.getInventory().getContents()) {
+            if(item == null) continue;
+
+            Map<String, Object> serializedItem = item.serialize();
+            playerItems.add(serializedItem);
+        }
+
+        playerInformation.put("inventory", playerItems);
+
+        Gson gson = new Gson();
+    }
+
+    /**
+     * Retrieves the player's dumped information
+     *
+     * If removeFromTemporaryFile is set, it will also remove them
+     * from the temporary file
+     */
+    public void getPlayerInformation(boolean removeFromTemporaryFile) {
 
     }
 }
