b670646094fef32be19d237644c96b61bc55a964
Connor Brady <connorbrady@codox.eu>
2024-06-25 15:33:55 +0100

    ADDED Initial files

---
+++ src/main/java/com/rexchoppers/mchunt/MCHunt.java
@@ -0,0 +1,17 @@
+package com.rexchoppers.mchunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCHunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/rexchoppers/mchunt/models/Arena.java
@@ -0,0 +1,7 @@
+package com.rexchoppers.mchunt.models;
+
+public class Arena {
+    private String id;
+    private String name;
+
+}
