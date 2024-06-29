2696b3b65c03f7b5701705a8f4fe30a8e161b929
Connor Brady <connorbrady@codox.eu>
2024-06-29 22:50:57 +0100

    ADDED Boundary rendering

--- src/main/java/com/rexchoppers/mchunt/tasks/ArenaSetupRenderBlocksTask.java
+++ src/main/java/com/rexchoppers/mchunt/tasks/ArenaSetupRenderBlocksTask.java
@@ -3,6 +3,7 @@ package com.rexchoppers.mchunt.tasks;
 import com.rexchoppers.mchunt.MCHunt;
 import com.rexchoppers.mchunt.managers.LocalizationManager;
 import com.rexchoppers.mchunt.models.ArenaSetup;
+import com.rexchoppers.mchunt.util.BoundaryUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -13,6 +14,8 @@ import static com.rexchoppers.mchunt.util.PlayerUtil.sendPlayerAudibleMessage;
 
 public class ArenaSetupRenderBlocksTask extends BukkitRunnable {
     private final MCHunt plugin;
+
+    private final BoundaryUtil boundaryUtil = new BoundaryUtil();
 
     public ArenaSetupRenderBlocksTask(MCHunt plugin) {
         this.plugin = plugin;
@@ -27,39 +30,37 @@ public class ArenaSetupRenderBlocksTask extends BukkitRunnable {
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    try {
-                        Player player = plugin.getServer().getPlayer(arenaSetup.getPlayerUuid());
+                    Player player = plugin.getServer().getPlayer(arenaSetup.getPlayerUuid());
 
-                        if (player == null || !player.isOnline()) {
-                            return;
-                        }
-
-                        // Render spawn blocks
-                        if (arenaSetup.getHiderSpawns() != null) {
-                            for (int i = 0; i < arenaSetup.getHiderSpawns().length; i++) {
-                                player.sendBlockChange(arenaSetup.getHiderSpawns()[i], plugin.getItemManager().itemArenaSetupHiderSpawn().getMaterial().createBlockData());
-                            }
-                        }
-
-                        if (arenaSetup.getSeekerSpawns() != null) {
-                            for (int i = 0; i < arenaSetup.getSeekerSpawns().length; i++) {
-                                player.sendBlockChange(arenaSetup.getSeekerSpawns()[i], plugin.getItemManager().itemArenaSetupSeekerSpawn().getMaterial().createBlockData());
-                            }
-                        }
-
-                        if (arenaSetup.getLobbySpawn() != null) {
-                            player.sendBlockChange(arenaSetup.getLobbySpawn(), plugin.getItemManager().itemArenaSetupLobbySpawn().getMaterial().createBlockData());
-                        }
-
-                        if (arenaSetup.getAfterGameSpawn() != null) {
-                            player.sendBlockChange(arenaSetup.getAfterGameSpawn(), plugin.getItemManager().itemArenaSetupAfterGameSpawn().getMaterial().createBlockData());
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        // sendPlayerAudibleMessage(plugin, Bukkit.getPlayer(arenaSetup.getPlayerUuid()), LocalizationManager.get("error.arenaSetupRenderBlocksTask"));
+                    if (player == null || !player.isOnline()) {
+                        return;
                     }
-                    // This part runs on the main thread
 
+                    // Render spawn blocks
+                    if (arenaSetup.getHiderSpawns() != null) {
+                        for (int i = 0; i < arenaSetup.getHiderSpawns().length; i++) {
+                            player.sendBlockChange(arenaSetup.getHiderSpawns()[i], plugin.getItemManager().itemArenaSetupHiderSpawn().getMaterial().createBlockData());
+                        }
+                    }
+
+                    if (arenaSetup.getSeekerSpawns() != null) {
+                        for (int i = 0; i < arenaSetup.getSeekerSpawns().length; i++) {
+                            player.sendBlockChange(arenaSetup.getSeekerSpawns()[i], plugin.getItemManager().itemArenaSetupSeekerSpawn().getMaterial().createBlockData());
+                        }
+                    }
+
+                    if (arenaSetup.getLobbySpawn() != null) {
+                        player.sendBlockChange(arenaSetup.getLobbySpawn(), plugin.getItemManager().itemArenaSetupLobbySpawn().getMaterial().createBlockData());
+                    }
+
+                    if (arenaSetup.getAfterGameSpawn() != null) {
+                        player.sendBlockChange(arenaSetup.getAfterGameSpawn(), plugin.getItemManager().itemArenaSetupAfterGameSpawn().getMaterial().createBlockData());
+                    }
+
+                    // Render boundary blocks
+                    if (arenaSetup.getLocationBoundaryPoint1() != null && arenaSetup.getLocationBoundaryPoint2() != null) {
+                        boundaryUtil.drawArenaBoundary(player, arenaSetup);
+                    }
                 }
             }.runTask(plugin);
         }
