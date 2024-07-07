bc536146f07a4503edc7e064c8f68645df15fee7
Connor Brady <connorbrady@codox.eu>
2024-07-07 20:21:53 +0100

    WIP Starting on Arena creation

--- src/main/java/com/rexchoppers/mchunt/models/Arena.java
+++ src/main/java/com/rexchoppers/mchunt/models/Arena.java
@@ -2,30 +2,107 @@ package com.rexchoppers.mchunt.models;
 
 import com.google.gson.annotations.Expose;
 import com.rexchoppers.mchunt.enums.ArenaStatus;
+import org.bukkit.Location;
+
+import java.util.UUID;
 
 public class Arena {
     @Expose
-    private String id;
+    private UUID uuid;
 
     @Expose
     private String name;
 
     private ArenaStatus status;
 
+    @Expose
+    private String regionId;
 
-    public Arena(String id, String name) {
-        this.id = id;
+    @Expose
+    private UUID createdByUuid;
+
+    @Expose
+    private Location[] arenaSigns;
+
+    @Expose
+    private String[] arenaBlocks;
+
+    @Expose
+    private Location lobbySpawn;
+
+    @Expose
+    private Location[] hiderSpawns;
+
+    @Expose
+    private Location[] seekerSpawns;
+
+    @Expose
+    private Location afterGameSpawn;
+
+    @Expose
+    private int minimumPlayers;
+
+    @Expose
+    private int maximumPlayers;
+
+    @Expose
+    private int seekerCount;
+
+    @Expose
+    private int countdownBeforeStart;
+
+    @Expose
+    private int countdownAfterEnd;
+
+    @Expose
+    private int respawnDelay;
+
+    @Expose
+    private Location locationBoundaryPoint1;
+
+    @Expose
+    private Location locationBoundaryPoint2;
+
+
+    public Arena(
+            UUID uuid,
+            String name,
+            String regionId,
+            UUID createdByUuid,
+            Location[] arenaSigns,
+            String[] arenaBlocks,
+            Location lobbySpawn,
+            Location[] hiderSpawns,
+            Location[] seekerSpawns,
+            Location afterGameSpawn,
+            int minimumPlayers,
+            int maximumPlayers,
+            int seekerCount,
+            int countdownBeforeStart,
+            int countdownAfterEnd,
+            int respawnDelay,
+            Location locationBoundaryPoint1,
+            Location locationBoundaryPoint2
+    ) {
+        this.uuid = uuid;
         this.name = name;
-        this.status = ArenaStatus.OFFLINE;
-    }
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+        this.status = ArenaStatus.WAITING;
+        this.regionId = regionId;
+        this.createdByUuid = createdByUuid;
+        this.arenaSigns = arenaSigns;
+        this.arenaBlocks = arenaBlocks;
+        this.lobbySpawn = lobbySpawn;
+        this.hiderSpawns = hiderSpawns;
+        this.seekerSpawns = seekerSpawns;
+        this.afterGameSpawn = afterGameSpawn;
+        this.minimumPlayers = minimumPlayers;
+        this.maximumPlayers = maximumPlayers;
+        this.seekerCount = seekerCount;
+        this.countdownBeforeStart = countdownBeforeStart;
+        this.countdownAfterEnd = countdownAfterEnd;
+        this.respawnDelay = respawnDelay;
+        this.locationBoundaryPoint1 = locationBoundaryPoint1;
+        this.locationBoundaryPoint2 = locationBoundaryPoint2;
     }
 
     public String getName() {
