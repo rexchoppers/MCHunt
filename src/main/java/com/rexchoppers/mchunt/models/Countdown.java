f8346b79aaa152b077fca92375f1e34ce007579a
Connor Brady <connorbrady@codox.eu>
2024-07-19 18:35:41 +0100

    ADDED Countdown model

---
+++ src/main/java/com/rexchoppers/mchunt/models/Countdown.java
@@ -0,0 +1,25 @@
+package com.rexchoppers.mchunt.models;
+
+public class Countdown {
+    private int countdown;
+
+    public Countdown(int countdown) {
+        this.countdown = countdown;
+    }
+
+    public int getCountdown() {
+        return countdown;
+    }
+
+    public void setCountdown(int countdown) {
+        this.countdown = countdown;
+    }
+
+public void decrementCountdown() {
+        countdown--;
+    }
+
+    public boolean isCountdownFinished() {
+        return countdown <= 0;
+    }
+}
