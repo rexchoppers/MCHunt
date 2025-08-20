000a912875030f495bb16107fef507cfd2a9cdbb
rexchoppers <connorbrady@codox.uk>
2025-08-20 19:58:02 +0100

    UPDATED Event for when user quits arena

--- src/main/java/com/rexchoppers/mchunt/listeners/PlayerLeftArenaListener.java
+++ src/main/java/com/rexchoppers/mchunt/listeners/PlayerLeftArenaListener.java
@@ -2,32 +2,32 @@ package com.rexchoppers.mchunt.listeners;
 
 import com.google.common.eventbus.Subscribe;
 import com.rexchoppers.mchunt.MCHunt;
+import com.rexchoppers.mchunt.enums.ArenaPlayerRole;
 import com.rexchoppers.mchunt.enums.ArenaStatus;
-import com.rexchoppers.mchunt.events.internal.ArenaSetupPlayerJoinedEvent;
-import com.rexchoppers.mchunt.events.internal.ArenaSetupUpdatedEvent;
 import com.rexchoppers.mchunt.events.internal.PlayerLeftArenaEvent;
 import com.rexchoppers.mchunt.managers.LocalizationManager;
 import com.rexchoppers.mchunt.models.Arena;
-import com.rexchoppers.mchunt.models.ArenaSetup;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import static com.rexchoppers.mchunt.util.PlayerUtil.sendPlayerAudibleMessage;
 import static com.rexchoppers.mchunt.util.PlayerUtil.sendPlayerMessage;
 
-public class PlayerLeftArenaListener {
-    private final MCHunt plugin;
-
-    public PlayerLeftArenaListener(MCHunt plugin) {
-        this.plugin = plugin;
-    }
+public record PlayerLeftArenaListener(MCHunt plugin) {
 
     @Subscribe
-    public void broadcastToPlayersInArena(PlayerLeftArenaEvent event) {
+    public void handleArenaWaiting(PlayerLeftArenaEvent event) {
         Arena arena = plugin.getArenaManager().getByUUID(event.arenaUuid()).orElse(null);
 
         if (arena == null) {
             return;
         }
+
+        if (!arena.getStatus().equals(ArenaStatus.WAITING)) {
+            return;
+        }
+
+        plugin.getSignManager().initArenaSigns(arena);
 
         arena.getPlayers().forEach(player -> {
             Player serverPlayer = plugin.getServer().getPlayer(player.getUUID());
@@ -46,18 +46,49 @@ public class PlayerLeftArenaListener {
     }
 
     @Subscribe
-    public void updateArenaSigns(PlayerLeftArenaEvent event) {
+    public void handleArenaInProgress(PlayerLeftArenaEvent event) {
         Arena arena = plugin.getArenaManager().getByUUID(event.arenaUuid()).orElse(null);
 
         if (arena == null) {
             return;
         }
 
-        plugin.getSignManager().initArenaSigns(arena);
+        if (!arena.getStatus().equals(ArenaStatus.IN_PROGRESS)) {
+            return;
+        }
+
+        // Announce player left the arena
+        arena.getPlayers().forEach(player -> {
+            Player serverPlayer = plugin.getServer().getPlayer(player.getUUID());
+
+            if (serverPlayer != null) {
+                sendPlayerAudibleMessage(
+                        serverPlayer,
+                        new LocalizationManager(MCHunt.getCurrentLocale())
+                                .getMessage("arena.player_left")
+                );
+            }
+        });
+
+        // Get count of hiders
+        int hiderCount = arena.getPlayers().stream()
+                .filter(player -> player.getRole().equals(ArenaPlayerRole.HIDER))
+                .mapToInt(player -> 1)
+                .sum();
+
+        if (hiderCount <= 0) {
+            // Declare seekers as winners
+            Bukkit.broadcastMessage("Not implemented yet: No hiders left in the arena, declaring seekers as winners.");
+        } else {
+            // Declare hiders as winners
+            Bukkit.broadcastMessage("Not implemented yet: Hiders left in the arena, declaring hiders as winners.");
+        }
+
     }
 
     /**
      * Cancel the start countdown if the player leaves the arena and the min player count is not met
+     * @param event
      */
     @Subscribe
     public void cancelStartCountdown(PlayerLeftArenaEvent event) {
@@ -76,7 +107,6 @@ public class PlayerLeftArenaListener {
         arena.setStatus(ArenaStatus.WAITING);
         arena.setStartCountdown(null);
 
-        plugin.getArenaManager().update(arena);
         plugin.getSignManager().initArenaSigns(arena);
 
         // Send players message that the countdown has been cancelled
