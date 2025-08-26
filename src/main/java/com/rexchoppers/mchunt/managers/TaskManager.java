ebfb48c1026d54ba79de977d0294d55110373b28
rexchoppers <connorbrady@codox.uk>
2025-08-26 13:54:13 +0100

    UPDATED Task manager register

--- src/main/java/com/rexchoppers/mchunt/managers/TaskManager.java
+++ src/main/java/com/rexchoppers/mchunt/managers/TaskManager.java
@@ -2,19 +2,29 @@ package com.rexchoppers.mchunt.managers;
 
 import com.rexchoppers.mchunt.MCHunt;
 import com.rexchoppers.mchunt.tasks.*;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class TaskManager {
-    private final MCHunt plugin;
+import java.util.HashMap;
 
-    public TaskManager(MCHunt plugin) {
-        this.plugin = plugin;
-    }
+public record TaskManager(MCHunt plugin) {
 
     public void registerTasks() {
-        new ArenaSetupItemVerificationTask(plugin).runTaskTimer(plugin, 0, 20 * 5);
-        new ArenaSetupRenderBlocksTask(plugin).runTaskTimer(plugin, 0, 20L);
-        new SignRenderTask(plugin).runTaskTimer(plugin, 0L, 5L);
-        new StartCountdownTask(plugin).runTaskTimer(plugin, 0L, 20L);
-        new ArenaTick(plugin).runTaskTimer(plugin, 0L, 20L);
+        HashMap<Class<?>, Long> tasks = new HashMap<>() {{
+            put(ArenaSetupItemVerificationTask.class, 20 * 5L);
+            put(ArenaSetupRenderBlocksTask.class, 20L);
+            put(SignRenderTask.class, 5L);
+            put(StartCountdownTask.class, 20L);
+            put(ArenaTick.class, 20L);
+        }};
+
+        for (Class<?> clazz : tasks.keySet()) {
+            try {
+                Long interval = tasks.get(clazz);
+                BukkitRunnable task = (BukkitRunnable) clazz.getConstructor(MCHunt.class).newInstance(plugin);
+                task.runTaskTimer(plugin, interval, interval);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to register task: " + clazz.getName(), e);
+            }
+        }
     }
 }
